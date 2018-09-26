0f176f2e3a10192d84aeef5f1c52a5b8793c3e45
sudip <sudipzoom@rediffmail.com>
2018-09-26 22:27:42 +0530

    Add header component and scss styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+ componentTitle="I am app component from  component.ts"
+
+ clickHandler(){
+ alert("I am clicked");
+
+ }
+
 }
---
+++ src/app/common/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+selector:'hector-header',
+templateUrl:'./header.component.html',
+styleUrls: ['./header.component.scss']
+})
+
+export class HeaderComponent{
+
+
+}
